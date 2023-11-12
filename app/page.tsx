3e49b6d53a829c86e2fce6536e6a102696d95993
IP <ilya.pavlovski@gmail.com>
2023-11-12 11:45:47 -0400

    Using gray-matter and dynamic homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+import fs from 'fs'
+import matter from 'gray-matter'
 import Link from 'next/link'
 import { css } from 'styled-system/css'
 import { Center, VStack } from 'styled-system/jsx'
 
-export default function Home() {
+export default async function Home() {
+  const paths = fs.readdirSync('./app/posts').map((dir) =>
+    fs.readdirSync(`./app/posts/${dir}`).map((post) => `/posts/${dir}/${post}`)
+  ).flat()
+  const posts = paths.map((path) => {
+    const source = fs.readFileSync(`./app${path}/page.mdx`)
+    const { data, content } = matter(source)
+    return {
+      path,
+      title: data.title as string,
+    }
+  })
+
   const styles = css({
     fontSize: '3xl',
     fontWeight: 'bold',
@@ -17,9 +31,13 @@ export default function Home() {
   // <div className={styles}>Hello panda 🐼!</div>
   return (
     <VStack height='100vh' background='slate.500' color='slate.100'>
-      <Link href='/mdx-test-1'>This is a test page</Link>
+      {posts.map(({ path, title }) => <Link href={path} key={path}>{title}</Link>)}
+      {
+        /* <Link href='/mdx-test-1'>This is a test page</Link>
       <Link href='/posts/2023-11/231111-01'>Nov 11, 2023 - first post</Link>
       <Link href='/posts/2023-11/231111-02'>Nov 11, 2023 - second post</Link>
+      <Link href='/posts/2023-11/231111-02'>{files[0]}</Link> */
+      }
     </VStack>
   )
 }
