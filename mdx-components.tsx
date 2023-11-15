dfe5f5660c4434145bcdb34ca8a42e82c4219b4a
IP <ilya.pavlovski@gmail.com>
2023-11-15 10:48:19 -0400

    Fix image width.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from '@mdx-js/react/lib'
 import type { MDXComponents } from 'mdx/types'
 import Image from 'next/image'
+import { DetailedHTMLProps, ImgHTMLAttributes } from 'react'
 import { css } from 'styled-system/css'
 
 function Header1({ children }: { children: ReactNode }) {
@@ -40,31 +41,44 @@ function Header2({ children }: { children: ReactNode }) {
 
 function Header3({ children }: { children: ReactNode }) {
   const styles = css({
-    // fontSize: '1rem',
     fontWeight: 'bold',
     textTransform: 'uppercase',
     marginTop: '1.7rem',
     marginBottom: '.8rem',
-    letterSpacing: 'wider'
+    letterSpacing: 'wider',
   })
 
   return <h3 className={styles}>{children}</h3>
 }
 
+// note: cover vs. fill, and h-value (200px, 400px, etc.)
+type ImgProps = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>
+function Img(props: ImgProps) {
+  const styles = css({
+    pos: 'relative',
+    display: 'block',
+    h: '200px',
+    w: 'full',
+    my: '1rem'
+  })
+
+  return (
+    <div className={styles}>
+      <Image
+        src={props.src!}
+        alt={props.alt!}
+        fill
+        style={{ objectFit: 'cover', borderRadius: '8px' }} />
+    </div>
+  )
+}
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h1: ({ children }) => <Header1 children={children} />,
-    h2: ({ children }) => <Header2 children={children} />,
-    h3: ({ children }) => <Header3 children={children} />,
-    img: (props) => (
-      // @ts-ignore
-      <Image
-        sizes='100vw'
-        {
-          // style={{ width: '100%', height: 'auto' }}
-          ...{ ...props, width: 500, height: 500 }
-        } />
-    ),
+    h1: ({ children }) => <Header1>{children}</Header1>,
+    h2: ({ children }) => <Header2>{children}</Header2>,
+    h3: ({ children }) => <Header3>{children}</Header3>,
+    img: (props) => <Img {...props} />,
     ...components,
   }
 }
