eab963eb4bf1358f26aeab10890483a45ce63707
IP <ilya.pavlovski@gmail.com>
2024-08-21 14:05:22 -0300

    Extract necessary metadata from id/slug.

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,10 +4,9 @@ import type { CollectionEntry } from 'astro:content'
 const postCollection = defineCollection({
   type: 'content',
   schema: z.object({
-    createdAt: z.string(),
     title: z.string(),
-    category: z.string(),
     description: z.string().optional(),
+    category: z.string(),
     tags: z.string().array().optional(),
     updated: z.object({ date: z.string(), note: z.string() }).array().optional(),
   }),
