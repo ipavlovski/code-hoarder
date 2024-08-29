776d706626bb202200fe982fdae341ac304d25da
IP <ilya.pavlovski@gmail.com>
2024-08-29 09:49:22 -0300

    Use native astro image component with config load.

--- src/components/mdx/image.tsx
+++ src/components/mdx/image.tsx
@@ -1,9 +1,23 @@
 import type { DetailedHTMLProps, ImgHTMLAttributes } from 'react'
 import styles from './mdx.module.css'
 
+import { images } from '../../content/config'
+
+
+import { Image } from 'astro:assets'
+
 // note: cover vs. fill, and h-value (200px, 400px, etc.)
 type ImgProps = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>
 export default function Img(props: ImgProps) {
+
+  console.log(images[props.src!])
+  if (images[props.src!]) {
+    // console.log(`image ${images[props.src!]} exists`)
+    const newPath = images[props.src!]()
+    console.log(`new path: ${newPath}`)
+  } else {
+    console.log(`image ${images[props.src!]} DOES NOT exist`)
+  }
 
   return (
     <div className={styles.imageWrapper}>
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,9 @@
 import { defineCollection, z } from 'astro:content'
 import type { CollectionEntry } from 'astro:content'
 
+import type { ImageMetadata } from 'astro';
+export const images = import.meta.glob<{ default: ImageMetadata }>('/src/content/posts/**/*.{jpeg,jpg,png,gif}');
+
 const postCollection = defineCollection({
   type: 'content',
   schema: z.object({
@@ -12,8 +15,11 @@ const postCollection = defineCollection({
   }),
 })
 
+console.log(images)
+
 export type PostDataProps = CollectionEntry<'posts'>['data']
 
 export const collections = {
   posts: postCollection,
 }
+
